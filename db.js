b40cdece71def90ab0b1a561fd32bf15b9eb21cd
JaeShinJun <jsstar1207@gmail.com>
2020-06-26 14:55:40 +0900

    pug, controller study

---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+    {
+        id:32355325,
+        title: 'Video awesome1',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 11111,
+            name: "James",
+            email: "jsstar1207@gmail.com"
+        }
+    },
+    {
+        id:124215,
+        title: 'Video awesome2',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 11111,
+            name: "James",
+            email: "jsstar1207@gmail.com"
+        }
+    },
+    {
+        id:666346,
+        title: 'Video awesome3',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 11111,
+            name: "James",
+            email: "jsstar1207@gmail.com"
+        }
+    },
+    {
+        id:7483863,
+        title: 'Video awesome4',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 11111,
+            name: "James",
+            email: "jsstar1207@gmail.com"
+        }
+    }
+]
