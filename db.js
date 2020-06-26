962d66d644da1b21eb0e80dbe27ba0c3f1b870f6
JaeShinJun <jsstar1207@gmail.com>
2020-06-26 15:07:00 +0900

    mixin study

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ export const videos = [
         title: 'Video awesome1',
         description: 'This is something I love',
         views: 24,
-        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
         creator: {
             id: 11111,
             name: "James",
@@ -16,7 +16,7 @@ export const videos = [
         title: 'Video awesome2',
         description: 'This is something I love',
         views: 24,
-        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
         creator: {
             id: 11111,
             name: "James",
@@ -28,7 +28,7 @@ export const videos = [
         title: 'Video awesome3',
         description: 'This is something I love',
         views: 24,
-        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
         creator: {
             id: 11111,
             name: "James",
@@ -40,7 +40,7 @@ export const videos = [
         title: 'Video awesome4',
         description: 'This is something I love',
         views: 24,
-        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
         creator: {
             id: 11111,
             name: "James",
