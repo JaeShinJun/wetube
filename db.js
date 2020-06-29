ae59c8e722189fff675a818ec2f6f9e926e3ae62
JaeShinJun <jsstar1207@gmail.com>
2020-06-30 00:32:25 +0900

    MongoDB study

--- db.js
+++ db.js
@@ -1,50 +1,18 @@
-export const videos = [
+import mongoose from "mongoose";
+
+mongoose.connect(
+    "mongodb://localhost:27017/wetube",
     {
-        id:32355325,
-        title: 'Video awesome1',
-        description: 'This is something I love',
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 11111,
-            name: "James",
-            email: "jsstar1207@gmail.com"
-        }
-    },
-    {
-        id:124215,
-        title: 'Video awesome2',
-        description: 'This is something I love',
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 11111,
-            name: "James",
-            email: "jsstar1207@gmail.com"
-        }
-    },
-    {
-        id:666346,
-        title: 'Video awesome3',
-        description: 'This is something I love',
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 11111,
-            name: "James",
-            email: "jsstar1207@gmail.com"
-        }
-    },
-    {
-        id:7483863,
-        title: 'Video awesome4',
-        description: 'This is something I love',
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 11111,
-            name: "James",
-            email: "jsstar1207@gmail.com"
-        }
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true
     }
-]
+);
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅ Connected to DB");
+const handleError = (error) => console.log(`❌ Error on DB Connection: ${error}`)
+
+db.once("open", handleOpen);
+db.on("error", handleError);
